14d6541f543c7d201d27bbcacf4c66235d26907e
Isac Lee <ilee29@jhmi.edu>
2019-05-13 16:11:49 -0400

    test.sh

--- test.sh
+++ test.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
-root=/kyber/Data/Nanopore/projects/nanonome/analysis/data/nanonome
-cell=MCF7
-bam=$root/pooled/bam/${cell}_nanoNOMe.pooled.bam
-cpg=$root/pooled/mbed/${cell}_nanoNOMe.pooled.cpg.meth.bed.gz
-gpc=$root/pooled/mbed/${cell}_nanoNOMe.pooled.gpc.meth.bed.gz
+root=/kyber/Data/Nanopore/Analysis/gilfunk/190508_recalledDATA_gup_flip/
+#cell=MCF7
+bam=$root/onTarg_mm2/min_gup3_prim_onTarg.bam
+cpg=$root/190510_methyLATION/isac_calls/min_gm12878_SORT_isac.mcalls.bed.gz
+#gpc=$root/pooled/mbed/${cell}_nanoNOMe.pooled.gpc.meth.bed.gz
 t=10
-fai=/mithril/Data/NGS/Reference/hg38_noalt/hg38_noalt.fa.fai
-out=$root/../../igv/${cell}_nanoNOMe.methylation.bam
-log=$root/../../igv/${cell}_nanoNOMe.methylation.log
+#fai=/mithril/Data/NGS/Reference/hg38_noalt/hg38_noalt.fa.fai
+bed=$root/meth_bed/methyL_2kb_flank.bed
+#out=$root/../../igv/${cell}_nanoNOMe.methylation.bam
+#log=$root/../../igv/${cell}_nanoNOMe.methylation.log
+out=$root/meth_plots/min_gm12878_isacHACK_isactry.bam
+ref=/mithril/Data/NGS/Reference/human38/GRCH38.fa
 
 python convert_bam_for_methylation.py --verbose -t $t \
-  -b $bam -c $cpg -g $gpc -a $fai -o $out &> $log
+  -f $ref -b $bam -c $cpg -r $bed -o $out
+
