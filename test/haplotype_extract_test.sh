5427024725f6dcda45e4b42096681600a5382f9b
Isac Lee <ilee29@jhmi.edu>
2019-05-27 15:43:14 -0400

    haplotyping test

--- test/haplotype_extract_test.sh
+++ test/haplotype_extract_test.sh
@@ -15,10 +15,17 @@ for hap in 1 2; do
   for mod in cpg gpc; do
     mbed=$(find $root/../../nanonome/pooled/mbed -name "*$cell*$mod*bed.gz") 
     out=$outdir/${cell}_nanoNOMe.$reg.$mod.hap$hap.meth.bed.gz
-    log=$outdir/${cell}_nanoNOMe.$reg.$mod.hap$hap.extract.log
-    python ../extract_mbed_by_qname.py --verbose -t $t \
-      -b $bam -m $mbed |\
-      sort -T $outdir -k1,1 -k2,2n | bgzip > $out
-    tabix -p bed $out
+    if [ ! -e $out ];then
+      # extract mbed
+      log=$outdir/${cell}_nanoNOMe.$reg.$mod.hap$hap.extract.log
+      python ../extract_mbed_by_qname.py --verbose -t $t \
+        -b $bam -m $mbed |\
+        sort -T $outdir -k1,1 -k2,2n | bgzip > $out
+      tabix -p bed $out
+    fi
+    # get mfreq
+    mfreq=$outdir/${cell}_nanoNOMe.$reg.$mod.hap$hap.mfreq.txt.gz
+    ../parseMethylbed.py frequency -v -i $out -m $mod |\
+      bgzip > $mfreq
   done
 done
